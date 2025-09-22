d3666689e5e3306821be3446b1f0d93dd24c8756
kingjay21 <mathewsmunemba41@gmail.com>
2025-09-22 11:05:24 +0100

    Day 7: Python input and output

---
+++ User_Profile.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+language = input("Enter your favorite programming language: ")
+hobby = input("Enter your hobby: ")
+print("/nHello", name + "!")
+print("YOU are:", age)
+print(f"Your favourite language is {language} and your hobby is {hobby}.")
+
